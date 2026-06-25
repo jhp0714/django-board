1d990f7686ca0aa0ba6d4c14b12fd5eb5a8b35b5
jhp0714 <gowns171@naver.com>
2026-06-25 20:11:56 +0900

    df

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,13 +1,15 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.36.39.13','boardhj.kro.kr','www.boardhj.kro.kr']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
+ALLOWED_HOSTS = ['3.36.39.13', 'boardhj.kro.kr', 'www.boardhj.kro.kr']
+
 DEBUG = False
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
