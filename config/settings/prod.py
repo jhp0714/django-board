c4337cc77e5e99d2f9e90135dd17258402d5fb2c
jhp0714 <gowns171@naver.com>
2026-07-04 21:19:30 +0900

    https 설정 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -30,3 +30,11 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
+SECURE_PROXY_SSL_HEADER = (
+    'HTTP_X_FORWARDED_PROTO',
+    'https',
+)
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
