8bdf17e6091ca2eaaa26e04744f89e4edfb930ab
jhp0714 <gowns171@naver.com>
2026-07-16 20:50:15 +0900

    회원가입 후 이동하는 url 수정

--- common/views/account_views.py
+++ common/views/account_views.py
@@ -17,7 +17,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect('board:redirect_to)question')
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form' : form})
